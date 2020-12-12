899d5c715fb2627501dbd5fae17dbe133b6a6536
Ren Aoki <aokiren.official@gmail.com>
2020-12-12 11:28:37 +0900

    Initial Commit

---
+++ Accelerometer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Accelerometer
+//
+//  Created by 青木蓮 on 2020/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
